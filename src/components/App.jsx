36ca3539ef123a2e5e4de10ed54b5e15884a4abb
AfanasevKirillGitHub <106386403+AfanasevKirillGitHub@users.noreply.github.com>
2023-01-04 12:01:04 +0200

    hw-03 phonebook complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -49,6 +49,22 @@ export class App extends Component {
     );
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem("contacts")
+    const parsedContacts = JSON.parse(savedContacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+
   render() {
     const { filter } = this.state;
     const normalizeContact = this.toLowerCaseFilter();
