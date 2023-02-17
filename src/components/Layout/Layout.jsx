b0a609f78d36236bd212ead2a7238e8603a9d726
AfanasevKirillGitHub <afanasev.dp@gmail.com>
2023-02-18 00:27:06 +0200

    fix delete comments

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,9 +2,6 @@ import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { Header } from 'components/Header/Header';
 
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
 export const Layout = () => {
   return (
     <>
@@ -12,7 +9,6 @@ export const Layout = () => {
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-      {/* <ToastContainer autoClose={2000} /> */}
     </>
   );
 };
